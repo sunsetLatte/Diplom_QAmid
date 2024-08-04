c9aeadc51aa065f62a6383ff36027ef051a96bdd
Julique <julikova.n@gmail.com>
2024-08-04 21:55:52 +0300

    add first test

---
+++ FMHandroid/app/src/androidTest/java/ru/iteco/fmhandroid/ui/TestUtils.java
@@ -0,0 +1,80 @@
+package ru.iteco.fmhandroid.ui;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import android.view.View;
+import androidx.test.espresso.PerformException;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.util.HumanReadables;
+import androidx.test.espresso.util.TreeIterables;
+import org.hamcrest.Matcher;
+import java.util.concurrent.TimeoutException;
+
+import ru.iteco.fmhandroid.R;
+
+
+public class TestUtils {
+    /**
+     * Perform action of waiting for a specific view id to be displayed.
+     * @param viewId The id of the view to wait for.
+     * @param millis The timeout of until when to wait for.
+     */
+    public static ViewAction waitDisplayed(final int viewId, final long millis) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isRoot();
+            }
+
+            @Override
+            public String getDescription() {
+                return "wait for a specific view with id <" + viewId + "> has been displayed during " + millis + " millis.";
+            }
+
+            @Override
+            public void perform(final UiController uiController, final View view) {
+                uiController.loopMainThreadUntilIdle();
+                final long startTime = System.currentTimeMillis();
+                final long endTime = startTime + millis;
+                final Matcher<View> matchId = withId(viewId);
+                final Matcher<View> matchDisplayed = isDisplayed();
+
+                do {
+                    for (View child : TreeIterables.breadthFirstViewTraversal(view)) {
+                        if (matchId.matches(child) && matchDisplayed.matches(child)) {
+                            return;
+                        }
+                    }
+
+                    uiController.loopMainThreadForAtLeast(50);
+                }
+                while (System.currentTimeMillis() < endTime);
+
+                // timeout
+                throw new PerformException.Builder()
+                        .withActionDescription(this.getDescription())
+                        .withViewDescription(HumanReadables.describe(view))
+                        .withCause(new TimeoutException())
+                        .build();
+            }
+        };
+    }
+
+        public static void logOut() {
+            ViewInteraction userButton = onView(withId(R.id.authorization_image_button));
+            userButton.check(matches(isDisplayed()));
+            userButton.perform(click());
+
+            ViewInteraction logOutButton = onView(withText("Log out"));
+            logOutButton.check(matches(isDisplayed()));
+            logOutButton.perform(click());
+        }
+}
